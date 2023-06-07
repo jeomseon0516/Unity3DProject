751f68122815e44040885d2425b05746f8bb5a40
icho117160 <icho117160@gmail.com>
2023-06-07 11:11:15 +0900

    ReUpdate

---
+++ 3DProject/Assets/MyScriptsPack/ObjectPoolManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class ObjectPoolManager : Singleton<ObjectPoolManager>, ISingleton
+{
+    public void Init()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private ObjectPoolManager() {}
+}
--- 3DProject/Assets/MyScriptsPack/Singleton.cs
+++ 3DProject/Assets/MyScriptsPack/Singleton.cs
@@ -2,16 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using SingletonBase;
 
-public interface ISingleton
+public interface ISingleton 
 {
     void Init();
 }
 
+namespace SingletonBase
+{
+    public abstract class SingletonBase<T> : MonoBehaviour where T : SingletonBase<T>
+    {
+        protected abstract void Awake();
+        protected SingletonBase() {}
+    }
+}
+
 // .. 모노비하이비어를 상속받는 싱글톤 템플릿 입니다. ISingleton의 함수를 정의하여 사용해야 합니다.
-public class Singleton<T> : MonoBehaviour where T : Singleton<T>, ISingleton
+//    Awake 오버라이딩시 정상적으로 동작하지 않습니다.
+//    Awake 대신 Init메서드를 오버라이딩 후 사용해주세요.
+
+public abstract class Singleton<T> : SingletonBase<Singleton<T>> where T : Singleton<T>, ISingleton
 {
     private static T instance = null;
+
     public static T GetInstance
     {
         get
@@ -21,7 +35,7 @@ public class Singleton<T> : MonoBehaviour where T : Singleton<T>, ISingleton
         }
     }
 
-    private void Awake()
+    protected override sealed void Awake()
     {
         if (CreateInstance(() => { TryGetComponent(out instance); }))
             Destroy(gameObject);
@@ -42,5 +56,6 @@ public class Singleton<T> : MonoBehaviour where T : Singleton<T>, ISingleton
 
         return true;
     }
-    protected Singleton() { }
+
+    protected Singleton() {}
 }
